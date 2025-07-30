8506986efe30ed6d40009adf54fe3e9f3d66c528
sandorjj <sandorjanosjonas@gmail.com>
2025-07-30 10:25:37 -0500

    fix: surrounded accessing variables with double quotes

--- arch-install.sh
+++ arch-install.sh
@@ -21,32 +21,32 @@ echo -e "\nWhat is the name of the disk you want to format and partition?"
 read disk
 
 efi=""
-while [[ ! $efi =~ [0-9]+ ]]; do
+while [[ ! "$efi" =~ [0-9]+ ]]; do
     echo -e "\nHow many GiB should the EFI system partition be? (recommended 1 GiB)"
     read efi
 
-    if [[ ! $efi =~ [0-9]+ ]]; then
+    if [[ ! "$efi" =~ [0-9]+ ]]; then
         echo -e "\"$efi\" is invalid! Try again."
     fi
 done
 
 swap=""
-while [[ ! $swap =~ [0-9]+ ]]; do
+while [[ ! "$swap" =~ [0-9]+ ]]; do
     echo -e "\nHow many GiB should the swap partition be? (recommended 4 GiB)"
     read swap
 
-    if [[ ! $swap =~ [0-9]+ ]]; then
-        echo -e "\"$swap\" is invalid! Try again."
+    if [[ ! "$swap" =~ [0-9]+ ]]; then
+        echo -e "\""$swap"\" is invalid! Try again."
     fi
 done
 
 root=""
-while [[ ! $root =~ [0-9]+ ]]; do
+while [[ ! "$root" =~ [0-9]+ ]]; do
     echo -e "\nHow many GiB should the root partition be? (recommended remainder (0) GiB)"
     read root
 
-    if [[ ! $root =~ [0-9]+ ]]; then
-        echo -e "\"$root\" is invalid! Try again."
+    if [[ ! "$root" =~ [0-9]+ ]]; then
+        echo -e "\""$root"\" is invalid! Try again."
     fi
 done
 
@@ -58,15 +58,15 @@ echo -e "Is the partition layout correct?"
 confirm
 
 echo -e "\nFormatting disk:"
-sgdisk /dev/$disk -o
+sgdisk /dev/"$disk" -o
 echo -e "\nCreating EFI system partition:"
-sgdisk /dev/$disk -n 0:0:+"$efi"GiB
-sgdisk /dev/$disk -t 1:EF00
+sgdisk /dev/"$disk" -n 0:0:+"$efi"GiB
+sgdisk /dev/"$disk" -t 1:EF00
 echo -e "\nCreating swap partition:"
-sgdisk /dev/$disk -n 0:0:+"$swap"GiB
-sgdisk /dev/$disk -t 2:8200
+sgdisk /dev/"$disk" -n 0:0:+"$swap"GiB
+sgdisk /dev/"$disk" -t 2:8200
 echo -e "\nCreating root partition:"
-sgdisk /dev/$disk -n 0:0:+"$root"
+sgdisk /dev/"$disk" -n 0:0:+"$root"
 
 echo ""
 lsblk
@@ -80,18 +80,18 @@ read root_name
 
 
 echo -e "\nFormatting EFI system partition:"
-mkfs.fat -F 32 /dev/$efi_name
+mkfs.fat -F 32 /dev/"$efi_name"
 echo -e "\nFormatting swap partition:"
-mkswap /dev/$swap_name
+mkswap /dev/"$swap_name"
 echo -e "\nFormatting root partition:"
-mkfs.ext4 /dev/$root_name
+mkfs.ext4 /dev/"$root_name"
 
 echo -e "\nMounting EFI system partition:"
-mount --mkdir /dev/$efi_name /mnt/boot
+mount --mkdir /dev/"$efi_name /mnt/boot"
 echo -e "\nEnabling swap partition:"
-swapon /dev/$swap_name
+swapon /dev/"$swap_name"
 echo -e "\nMounting root partition:"
-mount /dev/$root_name /mnt
+mount /dev/"$root_name" /mnt
 
 echo -e "\nCreating mirrorlist for downloads:"
 reflector --country Canada --latest 50 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
@@ -116,7 +116,7 @@ echo -e "\nWhat timezone are you in?"
 read timezone
 
 echo -e "\nSetting time:"
-ln -sf /usr/share/zoneinfo/$country/$timezone /etc/localtime
+ln -sf /usr/share/zoneinfo/"$country"/"$timezone" /etc/localtime
 hwclock --systohc
 
 echo -e "\nSetting localization:"
@@ -136,8 +136,8 @@ passwd
 echo -e "\nAdding user:"
 echo -e "What should the user's username be?"
 read username
-useradd -m -G wheel $username
-passwd $username
+useradd -m -G wheel "$username"
+passwd "$username"
 
 echo -e "\nEnabling NetworkManager on startup:"
 systemctl enable NetworkManager

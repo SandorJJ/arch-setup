ca07056058f123d9426af1a8b4ae7c167aa6440b
sandorjj <sandorjanosjonas@gmail.com>
2025-07-31 11:15:10 -0500

    more testing

--- arch-install.sh
+++ arch-install.sh
@@ -128,12 +128,9 @@ echo -e "\nSet root password:"
 arch-chroot /mnt passwd
 
 echo -e "\nSetting up bootloader (GRUB):"
-arch-chroot /mnt /bin/bash -e <<EOF
-pacman -S grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-EOF
+arch-chroot /mnt pacman -S grub efibootmgr
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 unmount -R /mnt
 

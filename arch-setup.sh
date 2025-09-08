d4870c7436fc8c0260ff9a9189b0c08186639801
sandorjj <sandorjanosjonas@gmail.com>
2025-09-08 12:38:49 -0500

    added a bunch of packages to install (needs cleanup)

--- arch-setup.sh
+++ arch-setup.sh
@@ -99,17 +99,48 @@ password="${input}"
 print_percentage 0 "Updating packages"
 echo "${password}" | sudo -S pacman -Syu
 
-utility_packages="vim git reflector"
-print_percentage 5 "Installing utility packages (${utility_packages})"
+utility_packages="vim git reflector vi fastfetch"
+print_percentage 5 "Installing utility tool packages (${utility_packages})"
 echo "${password}" | sudo -S pacman -S --noconfirm --needed ${utility_packages}
 
-print_percentage 10 "Installing yay"
+print_percentage 10 "Installing AUR package manager packages (yay go)"
+echo "${password}" | sudo -S pacman -S --noconfirm --needed go
 git clone https://aur.archlinux.org/yay.git
 cd yay/
-echo "${password}" | sudo -S makepkg -si --noconfirm --needed
+makepkg -si --noconfirm --needed
 cd
 rm -rf yay/
 
 documentation_packages="man-db man-pages tldr"
 print_percentage 15 "Installing documentation packages (${documentation_packages})"
 echo "${password}" | sudo -S pacman -S --noconfirm --needed ${documentation_packages}
+
+font_packages="ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols-mono"
+print_percentage 15 "Installing font packages (${font_packages})"
+echo "${password}" | sudo -S pacman -S --noconfirm --needed ${font_packages}
+
+desktop_packages="hyprland qt5-wayland qt6-wayland xdg-desktop-portal-hyprland hyprpolkitagent hyprpaper waybar sddm"
+print_percentage 20 "Installing desktop environment packages (${desktop_packages})"
+echo "${password}" | sudo -S pacman -S --noconfirm --needed ${desktop_packages}
+
+launcher_packages="gtk4 gtk4-layer-shell cairo protobuf poppler-glib"
+print_percentage 30 "Installing app launcher packages (walker elephant elephant-desktopapplications ${launcher_packages})"
+echo "${password}" | sudo -S pacman -S --noconfirm --needed ${launcher_packages}
+yay -S --noconfirm --needed elephant elephant-desktopapplications
+git clone https://github.com/abenz1267/walker.git
+cd walker/
+cd
+rm -rf walker/
+
+notification_packages="dunst libnotify"
+print_percentage 25 "Installing notification packages (${notification_packages})"
+echo "${password}" | sudo -S pacman -S --noconfirm --needed ${notification_packages}
+
+audio_packages="pipewire wireplumber pipewire-pulse pipewire-jack pavucontrol"
+print_percentage 30 "Installing audio packages (${audio_packages})"
+echo "${password}" | sudo -S pacman -S --noconfirm --needed ${audio_packages}
+
+other_packages="yazi dolphin obs-studio"
+print_percentage 25 "Installing other packages (${other_packages})"
+echo "${password}" | sudo -S pacman -S --noconfirm --needed ${other_packages}
+yay -S --noconfirm --needed wezterm-git zen-browser-bin
